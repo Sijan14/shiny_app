18fd230c5db89cee8802929bce4fff0ee80b745b
Sijan14 <sisijan18@earlham.edu>
2024-11-25 22:06:45 -0500

    add

--- first_shiny_app.R
+++ first_shiny_app.R
@@ -2,10 +2,21 @@ install.packages("shiny")
 library(shiny)
 
 ui <- fluidPage(
-  "Hello, world!"
+  selectInput(inputId = "dataset", label = "Dataset", choices = ls("package:datasets")),
+  verbatimTextOutput("summary"),
+  tableOutput("table")
 )
 
 server <- function(input, output, session) {
+  output$summary <- renderPrint({
+  dataset <- get(input$dataset, "package:datasets")
+  summary(dataset)
+  })
+  
+  output$table <- renderTable({
+    dataset <- get(input$dataset, "package:datasets")
+    dataset
+  })
 }
 
 shinyApp(ui, server)
